ac60d378728e24154edb886ea780c0cc2c8d48c3
Ali <shouni007@gmail.com>
2025-12-09 02:14:46 +0100

    Création de l'interface IRecordIterator

---
+++ src/bdda/IRecordIterator.java
@@ -0,0 +1,25 @@
+package bdda;
+
+import java.io.IOException;
+
+/**
+ * Interface pour parcourir un ensemble de records
+ */
+public interface IRecordIterator {
+    
+    /**
+     * Retourne le prochain record et avance le curseur
+     * @return le prochain record, ou null s'il n'y en a plus
+     */
+    Record GetNextRecord() throws IOException;
+    
+    /**
+     * Ferme l'iterateur et libere les ressources
+     */
+    void Close();
+    
+    /**
+     * Remet le curseur au debut
+     */
+    void Reset() throws IOException;
+}
